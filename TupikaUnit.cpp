18e256012c62fa0053e3f0fda7894d71874718ac
CaptainFinger <denis6077@gmail.com>
2018-04-03 20:04:21 +0300

    хм

--- TupikaUnit.cpp
+++ TupikaUnit.cpp
@@ -4,9 +4,11 @@
 #pragma hdrstop
  #include "AhmeTau.h"
  #include "GorbunoV.h"
-#include "TupikaUnit.h"
-#include "Sherstuk_form.h"
-#include "lab4.h"
+ #include "Unit4.h"
+ #include "SOboLEV.h"
+ #include "TupikaUnit.h"
+ #include "Sherstuk_form.h"
+ #include "lab4.h"
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
 #pragma resource "*.dfm"
@@ -25,7 +27,9 @@ for (int i = 1; i < 18; i++ ) {
 void __fastcall TTupikaforma::Button1Click(TObject *Sender)
 {
 	StringGrid1 -> Cells[1][1] = G_01_Akhmetov;
-    StringGrid1 -> Cells[1][3] = G_03_Gorbunov;
+	StringGrid1 -> Cells[1][3] = G_03_Gorbunov;
+	StringGrid1 -> Cells[1][5] = G_05_Zabelov;
+	StringGrid1->Cells[1][12] = G_12_Sobolev;
 	StringGrid1->Cells[1][13] = G_13_Tupika;
 	StringGrid1->Cells[1][15] = G_15_Shanin;
 	StringGrid1->Cells[1][16] = G_16_Sherstuk;
